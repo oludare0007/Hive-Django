391e3809238096acc1619e08bd9c8750aaad627b
oludare0007 <oludareayodele@gmail.com>
2024-01-24 14:48:09 +0100

    django

--- django_project/settings.py
+++ django_project/settings.py
@@ -130,6 +130,13 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS =True
+EMAIL_HOST_USER = os.environ.get('')
+EMAIL_HOST_PASSWORD = os.environ.get('')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
